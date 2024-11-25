91aa296ffe735a5f9d6402a05c107648d890d1fa
chenyuqiang <chenyuqiang@supcon.com>
2024-11-25 20:34:37 +0800

    feat: 新增tsconfig.json

--- docs/docs/.vitepress/components/vp-demo/vp-example.vue
+++ docs/docs/.vitepress/components/vp-demo/vp-example.vue
@@ -12,7 +12,7 @@
 
 <script lang="ts" setup>
 import { onBeforeMount, shallowRef } from 'vue'
-const props: any = defineProps<{
+const props = defineProps<{
   path?: string
 }>()
 // 创建一个跟踪自身 .value 变化的 ref，但不会使其值也变成响应式的。
@@ -21,13 +21,13 @@ onBeforeMount(() => {
   // 匹配到的文件默认是懒加载的，通过动态导入实现，并会在构建时分离为独立的 chunk。如果你倾向于直接引入所有的模块（例如依赖于这些模块中的副作用首先被应用），你可以传入 { eager: true } 作为第二个参数：
   const modules = import.meta.glob(`../../../examples/**/*.vue`, {
     eager: true
-  })
+  }) as Record<string, any>
 
   // 动态加载示列组件
   for (const modulesKey in modules) {
     const module = modules[modulesKey]
     // 找到example的组件，并加载
-    if (modulesKey.split('.vue')[0].endsWith(props.path)) {
+    if (modulesKey.split('.vue')[0].endsWith(props.path!)) {
       dynamicComponent.value = module.default
     }
   }
