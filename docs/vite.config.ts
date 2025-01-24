d27a47276c964e739f0120018710a2879af41355
chenyuqiang <chenyuqiang@supcon.com>
2025-01-24 14:01:42 +0800

    test(docs): 构建报错

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -7,18 +7,28 @@ export default defineConfig({
   plugins: [],
   resolve: {
     alias: [
-      ...(process.env.DOC_ENV === 'production'
-        ? []
-        : [
-            {
-              find: /^@jeffchen123\/sc-ui(\/(es|lib))?$/,
-              replacement: path.resolve(projRoot, 'packages/sc-ui/index.ts')
-            },
-            {
-              find: /^@jeffchen123\/sc-ui\/(es|lib)\/(.*)$/,
-              replacement: `${path.resolve(projRoot, 'packages')}/$2`
-            }
-          ])
+      // ...(process.env.DOC_ENV !== 'production'
+      //   ? []
+      //   : [
+      //       {
+      //         find: /^@jeffchen123\/sc-ui(\/(es|lib))?$/,
+      //         replacement: path.resolve(projRoot, 'packages/sc-ui/index.ts')
+      //       },
+      //       {
+      //         find: /^@jeffchen123\/sc-ui\/(es|lib)\/(.*)$/,
+      //         replacement: `${path.resolve(projRoot, 'packages')}/$2`
+      //       }
+      //     ])
+      ...[
+        {
+          find: /^@jeffchen123\/sc-ui(\/(es|lib))?$/,
+          replacement: path.resolve(projRoot, 'packages/sc-ui/index.ts')
+        },
+        {
+          find: /^@jeffchen123\/sc-ui\/(es|lib)\/(.*)$/,
+          replacement: `${path.resolve(projRoot, 'packages')}/$2`
+        }
+      ]
     ]
   }
 })
